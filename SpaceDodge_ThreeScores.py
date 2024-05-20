520dd08809113154e08dc3a9e59d6f2e38ac49f5
mc2025 <71651865+mc2025@users.noreply.github.com>
2024-05-20 11:08:31 -0400

    Add files via upload

--- SpaceDodge_ThreeScores.py
+++ SpaceDodge_ThreeScores.py
@@ -22,9 +22,9 @@ PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 PLAYER_VEL = 5
 
-# Asteroid constants
-STAR_WIDTH = 60
-STAR_HEIGHT = 40
+# Asteroid size range
+ASTEROID_MIN_SIZE = 30
+ASTEROID_MAX_SIZE = 100
 
 # Projectile constants
 PROJECTILE_WIDTH = 5
@@ -37,8 +37,7 @@ PLAYER_IMG = pygame.image.load("galaga.png").convert_alpha()
 PLAYER_IMG = pygame.transform.scale(PLAYER_IMG, (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 # Load asteroid image
-STAR_IMG = pygame.image.load("Asteroid.png").convert_alpha()
-STAR_IMG = pygame.transform.scale(STAR_IMG, (STAR_WIDTH, STAR_HEIGHT))
+ASTEROID_IMG = pygame.image.load("Asteroid.png").convert_alpha()
 
 # Initialize sound effects
 pygame.mixer.init()
@@ -75,7 +74,7 @@ class Explosion(pygame.sprite.Sprite):
             self.kill()
 
 # Function to draw objects on the game window
-def draw(player, projectiles, elapsed_time, stars, all_sprites, score):
+def draw(player, projectiles, elapsed_time, asteroids, all_sprites, score):
     global bg_y
     WIN.blit(BG, (0, bg_y))
     WIN.blit(BG, (0, bg_y - HEIGHT))
@@ -88,11 +87,11 @@ def draw(player, projectiles, elapsed_time, stars, all_sprites, score):
     score_text = FONT.render(f"Score: {score}", 1, "white")
     WIN.blit(score_text, (10, 70))  # Adjusted y-coordinate to move score area downwards
 
-    # Draw stars (asteroids)
-    for star in stars:
-        rotated_star = pygame.transform.rotate(star["image"], star["angle"])
-        rotated_rect = rotated_star.get_rect(center=star["rect"].center)
-        WIN.blit(rotated_star, rotated_rect.topleft)
+    # Draw asteroids
+    for asteroid in asteroids:
+        rotated_asteroid = pygame.transform.rotate(asteroid["image"], asteroid["angle"])
+        rotated_rect = rotated_asteroid.get_rect(center=asteroid["rect"].center)
+        WIN.blit(rotated_asteroid, rotated_rect.topleft)
 
     # Draw player
     WIN.blit(PLAYER_IMG, player.topleft)
@@ -108,7 +107,7 @@ def draw(player, projectiles, elapsed_time, stars, all_sprites, score):
     pygame.display.update()
 
 # Function to display death message and prompt to play again
-def show_death_message(tscore1,tscore2,tscore3):
+def show_death_message(tscore1, tscore2, tscore3):
     message = FONT.render("You died! Would you like to play again? (Y/N)", True, (255, 0, 0))
     WIN.blit(message, (WIDTH // 2 - message.get_width() // 2, HEIGHT // 2 - message.get_height() // 2 - 50))
     highscore = FONT.render("Top Scores", True, (255, 0, 0))
@@ -127,14 +126,11 @@ def show_death_message(tscore1,tscore2,tscore3):
                     return True  # Player wants to play again
                 elif event.key == pygame.K_n:
                     return False  # Player doesn't want to play again
-d = shelve.open('scores.txt')
-f = shelve.open('scorestwo.txt')
-h = shelve.open('scoresthree.txt')
 
 # Main game loop
 def main():
     global bg_y
-    
+
     while True:  # Loop to handle replay
         run = True
         paused = False
@@ -144,131 +140,133 @@ def main():
         clock = pygame.time.Clock()
         start_time = time.time()
         elapsed_time = 0
-        star_add_increment = 2000
-        star_count = 0
-        stars = []  
-        projectiles = []  
-        space_pressed = False  
-        shoot_delay = 0.2  
-        last_shot_time = 0  
+        asteroid_add_increment = 2000
+        asteroid_count = 0
+        asteroids = []
+        projectiles = []
+        space_pressed = False
+        shoot_delay = 0.2
+        last_shot_time = 0
 
         all_sprites = pygame.sprite.Group()  # Create a sprite group for explosions
 
-        global d
-        
-        score = 0  # Initialize score
+        with shelve.open('scores.txt') as d, shelve.open('scorestwo.txt') as f, shelve.open('scoresthree.txt') as h:
+            score = 0  # Initialize score
 
-        while run:
-            star_count += clock.tick(60)
-            elapsed_time = time.time() - start_time
+            while run:
+                asteroid_count += clock.tick(60)
+                elapsed_time = time.time() - start_time
 
-            if star_count > star_add_increment:
-                for _ in range(3):
-                    star_x = random.randint(0, WIDTH - STAR_WIDTH)
-                    star_angle = random.randint(0, 360)
-                    star_rect = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
-                    star_vel = random.randint(1, 5) 
-                    star = {"rect": star_rect, "angle": star_angle, "image": STAR_IMG.copy(), "vel": star_vel}
-                    stars.append(star)
-                star_add_increment = max(200, star_add_increment - 50)
-                star_count = 0
+                if asteroid_count > asteroid_add_increment:
+                    for _ in range(3):
+                        asteroid_width = random.randint(ASTEROID_MIN_SIZE, ASTEROID_MAX_SIZE)
+                        asteroid_height = random.randint(ASTEROID_MIN_SIZE, ASTEROID_MAX_SIZE)
+                        asteroid_x = random.randint(0, WIDTH - asteroid_width)
+                        asteroid_angle = random.randint(0, 360)
+                        asteroid_rect = pygame.Rect(asteroid_x, -asteroid_height, asteroid_width, asteroid_height)
+                        asteroid_vel = random.randint(1, 5)
+                        asteroid_image = pygame.transform.scale(ASTEROID_IMG.copy(), (asteroid_width, asteroid_height))
+                        asteroid = {"rect": asteroid_rect, "angle": asteroid_angle, "image": asteroid_image, "vel": asteroid_vel}
+                        asteroids.append(asteroid)
+                    asteroid_add_increment = max(200, asteroid_add_increment - 50)
+                    asteroid_count = 0
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False  # Set run to False to exit the main loop
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        paused = not paused  # Toggle pause state when 'ESCAPE' is pressed
-                    elif event.key == pygame.K_SPACE:
-                        space_pressed = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_SPACE:
-                        space_pressed = False
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False  # Set run to False to exit the main loop
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            paused = not paused  # Toggle pause state when 'ESCAPE' is pressed
+                        elif event.key == pygame.K_SPACE:
+                            space_pressed = True
+                    elif event.type == pygame.KEYUP:
+                        if event.key == pygame.K_SPACE:
+                            space_pressed = False
 
-            # Check if the game is paused
-            if paused:
-                pause_text = FONT.render("PAUSE", True, (255, 255, 255))
-                WIN.blit(pause_text, (WIDTH // 2 - pause_text.get_width() // 2, HEIGHT // 2 - pause_text.get_height() // 2))
-                pygame.time.delay(100) # Add a small delay to avoid high CPU usage during pause
-                continue  # Skip the rest of the loop while paused
+                # Check if the game is paused
+                if paused:
+                    pause_text = FONT.render("PAUSE", True, (255, 255, 255))
+                    WIN.blit(pause_text, (WIDTH // 2 - pause_text.get_width() // 2, HEIGHT // 2 - pause_text.get_height() // 2))
+                    pygame.time.delay(100)  # Add a small delay to avoid high CPU usage during pause
+                    continue  # Skip the rest of the loop while paused
 
-            current_time = time.time()
-            if space_pressed and current_time - last_shot_time > shoot_delay:
-                projectile = pygame.Rect(player.centerx - PROJECTILE_WIDTH // 2, player.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT)
-                projectiles.append(projectile)
-                SHOOT_SOUND.play()  
-                last_shot_time = current_time
+                current_time = time.time()
+                if space_pressed and current_time - last_shot_time > shoot_delay:
+                    projectile = pygame.Rect(player.centerx - PROJECTILE_WIDTH // 2, player.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT)
+                    projectiles.append(projectile)
+                    SHOOT_SOUND.play()
+                    last_shot_time = current_time
 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
-                player.x -= PLAYER_VEL
-            if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
-                player.x += PLAYER_VEL
-            if keys[pygame.K_UP] and player.y - PLAYER_VEL >= 0:
-                player.y -= PLAYER_VEL
-            if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
-                player.y += PLAYER_VEL
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+                    player.x -= PLAYER_VEL
+                if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+                    player.x += PLAYER_VEL
+                if keys[pygame.K_UP] and player.y - PLAYER_VEL >= 0:
+                    player.y -= PLAYER_VEL
+                if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
+                    player.y += PLAYER_VEL
 
-            if player_alive:  # Only update game logic if the player is alive
-                for star in stars[:]:
-                    star["rect"].y += star["vel"]
-                    if star["rect"].y > HEIGHT:
-                        stars.remove(star)
-                    elif star["rect"].colliderect(player):
-                        stars.remove(star)
-                        HIT_SOUND.play()
-                        player_alive = False
-                        break
-                for projectile in projectiles[:]:
-                    projectile.y -= PROJECTILE_VEL
-                    if projectile.y + PROJECTILE_HEIGHT < 0:
-                        projectiles.remove(projectile)
-                    else:
-                        for star in stars[:]:
-                            if star["rect"].colliderect(projectile):
-                                # Create an explosion at the asteroid's position
-                                explosion = Explosion(star["rect"].centerx, star["rect"].centery, size=2)
-                                # Add the explosion to a sprite group for updating and rendering
-                                all_sprites.add(explosion)
-                                # Remove the asteroid and projectile
-                                stars.remove(star)
-                                projectiles.remove(projectile)
-                                HIT_SOUND.play()
-                                # Increase the score when an asteroid is destroyed
-                                score += 1
-                                break  
-                        else:  # This else clause is executed if no collision occurred
-                            continue  # Skip the rest of the loop
-                        break  # Break the loop if collision occurred
+                if player_alive:  # Only update game logic if the player is alive
+                    for asteroid in asteroids[:]:
+                        asteroid["rect"].y += asteroid["vel"]
+                        if asteroid["rect"].y > HEIGHT:
+                            asteroids.remove(asteroid)
+                        elif asteroid["rect"].colliderect(player):
+                            asteroids.remove(asteroid)
+                            HIT_SOUND.play()
+                            player_alive = False
+                            break
+                    for projectile in projectiles[:]:
+                        projectile.y -= PROJECTILE_VEL
+                        if projectile.y + PROJECTILE_HEIGHT < 0:
+                            projectiles.remove(projectile)
+                        else:
+                            for asteroid in asteroids[:]:
+                                if asteroid["rect"].colliderect(projectile):
+                                    # Create an explosion at the asteroid's position
+                                    explosion = Explosion(asteroid["rect"].centerx, asteroid["rect"].centery, size=2)
+                                    # Add the explosion to a sprite group for updating and rendering
+                                    all_sprites.add(explosion)
+                                    # Remove the asteroid and projectile
+                                    asteroids.remove(asteroid)
+                                    projectiles.remove(projectile)
+                                    HIT_SOUND.play()
+                                    # Increase the score when an asteroid is destroyed
+                                    score += 1
+                                    break
+                            else:  # This else clause is executed if no collision occurred
+                                continue  # Skip the rest of the loop
+                            break  # Break the loop if collision occurred
 
-                for star in stars:
-                    star["angle"] += 1
+                    for asteroid in asteroids:
+                        asteroid["angle"] += 1
 
-            bg_y = (bg_y + 1) % HEIGHT
+                bg_y = (bg_y + 1) % HEIGHT
 
-            draw(player, projectiles, elapsed_time, stars, all_sprites, score)
+                draw(player, projectiles, elapsed_time, asteroids, all_sprites, score)
 
-            # Update explosion animations
-            all_sprites.update()
+                # Update explosion animations
+                all_sprites.update()
 
-            if not player_alive:  # If player is dead, show death message and exit after a delay
-                if d['score'] < score:
-                    d['score'] = score
-                elif f['score'] < score:
-                    f['score'] = score
-                elif h['score'] < score:
-                    h['score'] = score
-                topscore = d['score']
-                secscore = f['score']
-                triscore = h['score']
-                if not show_death_message(topscore,secscore,triscore):  # If player doesn't want to play again
-                    run = False
-                    break  # Exit game loop
-                else:  # Player wants to play again
-                    break  # Exit inner loop to restart the game
+                if not player_alive:  # If player is dead, show death message and exit after a delay
+                    if d['score'] < score:
+                        d['score'] = score
+                    elif f['score'] < score:
+                        f['score'] = score
+                    elif h['score'] < score:
+                        h['score'] = score
+                    topscore = d['score']
+                    secscore = f['score']
+                    triscore = h['score']
+                    if not show_death_message(topscore, secscore, triscore):  # If player doesn't want to play again
+                        run = False
+                        break  # Exit game loop
+                    else:  # Player wants to play again
+                        break  # Exit inner loop to restart the game
 
-        if not run:  # If player doesn't want to play again, exit outer loop
-            break
+            if not run:  # If player doesn't want to play again, exit outer loop
+                break
 
     pygame.quit()
 
